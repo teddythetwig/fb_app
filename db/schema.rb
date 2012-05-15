8f030f5e2b0ab68fb37caa6d239c0c327c8a32a1
Edward Funger <teddythetwig@gmail.com>
2012-05-15 13:59:10 -0400

    added friendships model for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515165944) do
+ActiveRecord::Schema.define(:version => 20120515175831) do
+
+  create_table "friendships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "provider"
